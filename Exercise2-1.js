f36876858050f2eea999008d6080c835b16ce83d
riteshrohilla <65576858+riteshrohilla@users.noreply.github.com>
2023-01-10 12:35:36 +0530

    Create Exercise2-1.js

---
+++ Exercise2-1.js
@@ -0,0 +1,66 @@
+/**
+Challenge: 
+
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+/**
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+*/
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" width="50px" />
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+        <Header />
+            <h1>Reasons why I am excited to learn ReactJS: </h1>        
+                <ol>
+                    <li>It can make me employable</li>
+                    <li>It is easier to learn</li>
+                    <li>It is regulary maintained</li>
+                </ol>
+            
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <div>
+            <MainContent />
+            <footer>
+                    <small>
+                        © 2023 Rohilla Development. All rights reserved.
+                    </small>
+                </footer>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Footer/>)
