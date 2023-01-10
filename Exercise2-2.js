f7a848a0903041b25b7eee52adaf2caaa1275744
riteshrohilla <65576858+riteshrohilla@users.noreply.github.com>
2023-01-10 15:46:09 +0530

    Create Exercise2-2.js

---
+++ Exercise2-2.js
@@ -0,0 +1,56 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" className="nav-logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons why I am excited to learn ReactJS: </h1>        
+                <ol>
+                    <li>It can make me employable</li>
+                    <li>It is easier to learn</li>
+                    <li>It is regulary maintained</li>
+                </ol>
+            
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                    <small>
+                        © 2023 Rohilla Development. All rights reserved.
+                    </small>
+                </footer>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Page/>)
